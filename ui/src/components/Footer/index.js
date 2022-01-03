4256aed13946bfbac6ab732f3635a86f803a879e
derivativedegen <derivativedegen@gmail.com>
2022-01-03 01:49:33 -0600

    added logo to footer

--- ui/src/components/Footer/index.js
+++ ui/src/components/Footer/index.js
@@ -5,13 +5,22 @@ import { Link } from "react-router-dom";
 import { links } from "../../constants/constants";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import EmailIcon from "@mui/icons-material/Email";
+import solens_logo from "../../assets/images/logo2.png";
 
 export default function Footer(props) {
   const year = new Date().getFullYear();
 
   return (
-    <div className="footer col-12 d-flex flex-column justify-content-between">
+    <div className="footer col-12 d-flex flex-column justify-content-between align-items-center">
+      <div className="col-12 d-flex justify-content-center pt-3">
+        <img src={solens_logo} className="footer_logo" />
+      </div>
+
       <div className="footer_content col-12 d-flex flex-row flex-wrap justify-content-center mb-4">
+        <div className="d-flex d-md-none justify-content-center col-12">
+          <hr style={{ color: "white", width: "50%" }} className="" />
+        </div>
+
         <div className="footer_section col-12 col-md-3 d-flex flex-column align-items-center justify-content-around mb-2 mb-lg-0">
           <h4 className="footer_title">SITEMAP</h4>
           <Link to="/" style={{ textDecoration: "none" }}>
