bd0a48b4dde81c6515077c0ecc48f996a615fbf8
derivativedegen <derivativedegen@gmail.com>
2022-01-11 20:31:11 -0600

    added SMB market fee details

--- ui/src/components/ItemDetails/index.js
+++ ui/src/components/ItemDetails/index.js
@@ -1,9 +1,20 @@
+import React from "react";
 import { shortenAddress } from "../../candy-machine";
 import { explorerLink } from "../../constants/constants";
 
 export default function ItemDetails(props) {
-  const { invalid, item, royalty, received, tokenAccount, ownerAccount } =
-    props;
+  const {
+    invalid,
+    item,
+    royalty,
+    received,
+    tokenAccount,
+    ownerAccount,
+    marketplaces,
+  } = props;
+  const solanart = marketplaces.filter((item) => item === "solanart").length;
+  const magiceden = marketplaces.filter((item) => item === "magiceden").length;
+  const smb = marketplaces.filter((item) => item === "smb").length;
 
   return (
     <div className="d-flex flex-row flex-wrap col-12">
@@ -54,20 +65,28 @@ export default function ItemDetails(props) {
           {invalid && <span className="mint_info_value">Invalid Token</span>}
         </h5>
       </div>
-      <div className="col-12 col-lg-6 d-flex flex-column align-items-start justify-content-between">
-        <h5 className="mint_info">
-          Project Fee:{" "}
-          <span className="mint_info_value">
-            {!invalid ? royalty + "%" : "Invalid Token"}
-          </span>
-        </h5>
-        <h5 className="mint_info">
-          Solanart Fee: <span className="mint_info_value">3%</span>
-        </h5>
-
-        <h5 className="mint_info">
-          Magic Eden Fee: <span className="mint_info_value">2%</span>
-        </h5>
+      <div className="col-12 col-lg-6 d-flex flex-column align-items-start justify-content-start">
+        {smb === 0 && (
+          <h5 className="mint_info">
+            Project Fee:{" "}
+            <span className="mint_info_value">{royalty.toFixed(2)}%</span>
+          </h5>
+        )}
+        {smb > 0 && (
+          <h5 className="mint_info">
+            SMB Market Fee: <span className="mint_info_value">5%</span>
+          </h5>
+        )}
+        {solanart > 0 && (
+          <h5 className="mint_info">
+            Solanart Fee: <span className="mint_info_value">3%</span>
+          </h5>
+        )}
+        {magiceden > 0 && (
+          <h5 className="mint_info">
+            Magic Eden Fee: <span className="mint_info_value">2%</span>
+          </h5>
+        )}
       </div>
     </div>
   );
