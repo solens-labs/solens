5cbab29a9058ce7b35bfa8b038182c9ef33debaa
headmaster <arnori@protonmail.com>
2022-01-19 17:30:10 -0500

    Update solanart cron job to include txs and have halt functionality

--- cron/solanart/process.ts
+++ cron/solanart/process.ts
@@ -2,14 +2,17 @@
 import {Connection, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
 import {BN} from "@project-serum/anchor";
 import axios from "axios";
+import { exit } from 'process';
 
 const BASE58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 const base58 = require("base-x")(BASE58);
 
 const connection = new Connection('https://dawn-divine-feather.solana-mainnet.quiknode.pro/971e7d86ae5ad52d9d48097afaec1f7edde191e7/')
 const SOLANART = new PublicKey('CJsLwbP1iu5DuUikHEJnLfANgKy6stB2uFgvBBHoyxwz')
+const BPF_UPGRADABLE = new PublicKey('BPFLoaderUpgradeab1e11111111111111111111111')
 const TRANSACTION_URI = 'http://localhost:3000/transactions'
 const COLLECTION_URI = 'http://localhost:3000/collections/'
+const STATUS_URI = 'http://localhost:3000/status'
 
 
 class Assignable {
@@ -59,7 +62,7 @@ function getMintFromTx(tokenBalances, index) {
   return mint
 }
 
-function getListTxInfo(tx, ix) {
+function getListTxInfo(tx, ix, index) {
   let accounts = ix.accounts;
   let res = new tx_info({
     mint: accounts[4].toBase58(),
@@ -70,11 +73,12 @@ function getListTxInfo(tx, ix) {
     escrow: accounts[3].toBase58(),
     type: 'list',
     tx: tx.transaction.signatures[0],
+    ix: index
   })
   return res
 }
 
-function getBuyTxInfo(tx, ix) {
+function getBuyTxInfo(tx, ix, index) {
     let accountKeys = tx.transaction.message.accountKeys
     let accounts = ix.accounts;
     let mintIdx;
@@ -104,11 +108,12 @@ function getBuyTxInfo(tx, ix) {
         escrow: accounts[4].toBase58(),
         type: type,
         tx: tx.transaction.signatures[0],
+        ix: index
     })
     return res
 }
 
-function getUpdateTxInfo(tx, ix) {
+function getUpdateTxInfo(tx, ix, index) {
 
     let accounts = ix.accounts;
 
@@ -121,35 +126,56 @@ function getUpdateTxInfo(tx, ix) {
         escrow: accounts[1].toBase58(),
         type: 'update',
         tx: tx.transaction.signatures[0],
+        ix: index
     })
     return res
 }
 
-async function processLatestTxs(latestTxs) {
-    let payloads = []
-    latestTxs.forEach((tx) => {
-        tx.transaction.message.instructions.forEach((ix) => {
-            if (ix.programId.toBase58() == SOLANART.toBase58()) {
-                let ixData = base58.decode(ix.data)
-                let type = new BN(ixData[0]).toNumber()
-                switch (type) {
-                    case 4:
-                        payloads.push(getListTxInfo(tx, ix))
-                        break;
-                    case 5:
-                        payloads.push(getBuyTxInfo(tx, ix))
-                        break;
-                    case 6:
-                        payloads.push(getUpdateTxInfo(tx, ix))
-                        break;
-                    default:
-                        break;
-                }
+async function halt() {
+  await axios.post(STATUS_URI,
+    {"live": false},
+    { headers: { "content-type": "application/json", "Accept": "applicatin/json" }})
+}
 
-            }
-        })
-    })
-    return payloads
+function checkUpgrade(ix) {
+    return ix.programId.toBase58() == BPF_UPGRADABLE.toBase58() && ix.parsed.info.programAccount == SOLANART.toBase58()
+}
+
+async function processLatestTxs(latestTxs) {
+  let payloads = []
+  for (let i = 0; i < latestTxs.length; i++) {
+    let tx = latestTxs[i]
+
+    for (let index = 0; index < tx.transaction.message.instructions.length; index++) {
+        let ix = tx.transaction.message.instructions[index]
+
+        if (checkUpgrade(ix)) {
+          await halt()
+          return exit(1)
+        } else if (ix.programId.toBase58() == SOLANART.toBase58()) {
+              let ixData = base58.decode(ix.data)
+              let type = new BN(ixData[0]).toNumber()
+              switch (type) {
+                  case 2:
+                      payloads.push(getAcceptOfferTxInfo(tx, ix, index))
+                      break;
+                  case 4:
+                      payloads.push(getListTxInfo(tx, ix, index))
+                      break;
+                  case 5:
+                      payloads.push(getBuyTxInfo(tx, ix, index))
+                      break;
+                  case 6:
+                      payloads.push(getUpdateTxInfo(tx, ix, index))
+                      break;
+                  default:
+                      break;
+              }
+
+          }
+      }
+  }
+  return payloads
 }
 
 
@@ -192,7 +218,7 @@ async function process(until) {
 }
 
 async function main() {
-  let until = '1642284445'
+  let until = '1642631162'
   while (true) {
     let prev_until = until
     try {
