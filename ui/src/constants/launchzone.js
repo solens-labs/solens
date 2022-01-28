dabedecc425bdc4a50af414f5dc527d4e93037b2
derivativedegen <derivativedegen@gmail.com>
2022-01-27 22:32:56 -0600

    changed upcoming images to twitter pics

--- ui/src/constants/launchzone.js
+++ ui/src/constants/launchzone.js
@@ -1,30 +1,29 @@
-import bucketheadz from "../assets/images/launchzone/bucketheadsnft.jpg";
-import solanacuties from "../assets/images/launchzone/solanacutiez.jpg";
-import clubcrypto from "../assets/images/launchzone/clubcryptonft.jpg";
-
 export const launch_collections = [
   {
     name: "Bucketheadz",
-    image: bucketheadz,
+    image:
+      "https://pbs.twimg.com/profile_images/1480955362274537473/llPyMiJp_400x400.jpg",
     date: "2022-02-01T12:00:00",
     website: "https://bucketheads.io/",
     twitter: "https://twitter.com/bucketheadsnft",
-    discord: "",
+    discord: "https://discord.gg/PA6TQQTnwW",
   },
   {
     name: "Solana Cutiez",
-    image: solanacuties,
+    image:
+      "https://pbs.twimg.com/profile_images/1482662626631036936/7k_aon9Z_400x400.jpg",
     date: "2022-02-03T12:00:00",
     website: "https://cutiezheaven.pantherbyte.eu/",
     twitter: "https://twitter.com/solanacutiez",
-    discord: "",
+    discord: "https://discord.gg/CfHKGqgXwH",
   },
   {
     name: "Club Crypto",
-    image: clubcrypto,
+    image:
+      "https://pbs.twimg.com/profile_images/1463858752344395778/_VC73V2e_400x400.jpg",
     date: "2022-02-18T12:00:00",
     website: "https://www.clubcrypto.social/",
     twitter: "https://twitter.com/clubcryptonft",
-    discord: "",
+    discord: "https://discord.gg/XABKGHVJgV",
   },
 ];
