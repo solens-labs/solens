18d33f8457821bdae449cc4c689287df631ab7d7
derivativedegen <derivativedegen@gmail.com>
2022-05-01 15:05:14 -0500

    added rhino savannah

--- ui/src/constants/launchzone.js
+++ ui/src/constants/launchzone.js
@@ -4,7 +4,7 @@ export const launch_collections = [
   {
     name: "Bucketheads",
     image:
-      "https://pbs.twimg.com/profile_images/1480955362274537473/llPyMiJp_400x400.jpg",
+      "https://pbs.twimg.com/profile_images/1516575312938946561/mcHwuu-7_400x400.jpg",
     // candyMachineID: "6i3o15EjQerzgUqhaeWSAoPZPpAx7EpXPSfCxjVUaFX1",
     candyMachineID: "FMsX2Yv7NqqATxYdja9NLpyRHvjGpvXg6n8rUezysxiJ",
     launchDate: 1643749200000,
@@ -24,114 +24,31 @@ export const launch_collections = [
     80’s, the project is a forerunner for exciting projects yet to come, think sci-fi 
     and superheroes.`,
   },
-  // {
-  //   name: "Club Crypto",
-  //   image: "http://arweave.net/FT22FtqdTb79PcBMV63mG-35SD2ofV1-fkP57M5B7vo",
-  //   candyMachineID: "yszesogBdEQevowM24frrWu9TrShiepF6fyTzEDfJ1N",
-  //   wl: true,
-  //   wlToken: "epHTbjLiF3Bj5aJ2JbyM55J2xTSAmCkwasCSaduW9Xg",
-  //   launchDate: 1645387200000,
-  //   publicDate: 1645473600000,
-  //   endDate: undefined,
-  //   website: "https://www.clubcrypto.social/",
-  //   twitter: "https://twitter.com/clubcryptonft",
-  //   discord: "https://discord.gg/XABKGHVJgV",
-  //   symbol: "club_crypto",
-  //   supply: 4702,
-  //   preminted: 258,
-  //   price: 0.44,
-  //   wlPrice: 0.4,
-  //   description: `Club Crypto is a collection of 4,702 algorithmically generated isometric
-  //   characters, with a mission to educate and introduce new users to Web3 and blockchain
-  //   through social, P2E game mechanics and Metaverse integration. 258 exclusive Founder
-  //   Edition NFT have already been minted by early adopters! Don't miss your chance to
-  //   join the Club.`,
-  // },
-  // {
-  //   name: "METAMUSKVERSE",
-  //   image:
-  //     "https://pbs.twimg.com/profile_images/1491539129578012677/HXOSEeJj_400x400.jpg",
-  //   candyMachineID: "3vwMh1tuQDTbdZpPHZVWcmq9bvubUeMbQWrrKjAQbJzH",
-  //   launchDate: 1646946000000,
-  //   endDate: undefined,
-  //   website: "https://www.metamuskverse.space/",
-  //   twitter: "https://twitter.com/metamuskverse",
-  //   discord: "https://discord.gg/Txnhtbt4R9",
-  //   symbol: "metamuskverse",
-  //   supply: 3333,
-  //   preminted: 111,
-  //   price: 1,
-  //   description: `Welcome to the Metamuskverse. 3333 Metamuskverse NFT holders will be
-  //   the first inhabitants of the planet Mars in the multiplayer, play to earn (P2E),
-  //   virtual reality (VR) game on Avalanche and Solana. Users will get to explore and
-  //   discover the beautiful but dangerous universe, while fighting to earn planets and
-  //   galaxies to expand their metaverse experience in an endless universe.`,
-  // },
-  // {
-  //   name: "Rhino Savanna NFT",
-  //   image: "https://arweave.net/DFc3OQlV2hW9Jdy_Tspv1SYtvyDgW6UUgawxoXsaSSM",
-  //   candyMachineID: "",
-  //   wl: false,
-  //   wlToken: "",
-  //   launchDate: 1648314000000,
-  //   publicDate: 1648400400000,
-  //   endDate: undefined,
-  //   website: "https://rhinosavanna.earth/",
-  //   twitter: "https://twitter.com/RhinoSavannaNFT",
-  //   discord: "https://discord.gg/RHINOSAVANNANFT",
-  //   symbol: "rhino_savanna",
-  //   supply: 8933,
-  //   preminted: 0,
-  //   price: "TBD",
-  //   wlPrice: "TBD",
-  //   description: `Rhino Savanna is a project with a big heart, aimed at promoting awareness
-  //   of endangered species and climate change. With over 8933 passionately designed Rhinos,
-  //   incorporating cultures and countries from all over the world, owning a Rhino symbolizes
-  //   the mentality of global oneness.
-
-  //   We're partnered with the Wildlife Conservation Network's Rhino Recovery Fund and
-  //   International Rhino Foundation. We'll be donating 10% of Mint price for EACH minted
-  //   NFT to be used towards Rhino protection, health and habitats!`,
-  // },
-  // {
-  //   name: "Dinosaur Gentleman",
-  //   image: "https://www.dinogents.com/images/main.gif",
-  //   candyMachineID: "",
-  //   wl: true,
-  //   wlToken: "epHTbjLiF3Bj5aJ2JbyM55J2xTSAmCkwasCSaduW9Xg",
-  //   launchDate: 1649350800000,
-  //   publicDate: 1649437200000,
-  //   endDate: undefined,
-  //   website: "https://www.dinogents.com/",
-  //   twitter: "https://twitter.com/dinogentsNFT/",
-  //   discord: "https://discord.gg/t8qjytpQ8d",
-  //   symbol: "dinogents",
-  //   supply: 500,
-  //   preminted: 0,
-  //   price: 0.5,
-  //   wlPrice: 0,
-  //   description: `Dinogents is built on Solana Blockchain for both Arts and
-  //   Token $DNG with the most standard-based community in the Crypto Space.`,
-  // },
   {
-    name: "Dude Pixels",
-    image:
-      "https://cdn.discordapp.com/attachments/876610280531066880/957049779429457960/IMG_1090.jpg",
-    candyMachineID: "",
+    name: "Rhino Savanna NFT",
+    image: "https://arweave.net/DFc3OQlV2hW9Jdy_Tspv1SYtvyDgW6UUgawxoXsaSSM",
+    candyMachineID: "72ktUeBDSjKJuJWL9KxKYodMBE1zYQJ5UrdMkKzACema",
     wl: false,
-    wlToken: "",
-    launchDate: 1650474000000,
-    publicDate: 1650474000000,
+    wlToken: "F9LEgZvh4NnLdyGt8anJ44JjgKTW5bpkcfUdGA9UFXzz",
+    launchDate: 1651525200000,
+    publicDate: 1651525200000,
     endDate: undefined,
-    website: "",
-    twitter: "",
-    discord: "",
-    symbol: "pixeldudes",
-    supply: 3333,
-    preminted: 0,
-    price: 1,
-    wlPrice: 0,
-    description: `Dude, it's pixels all over the place.`,
+    website: "https://rhinosavanna.earth/",
+    twitter: "https://twitter.com/RhinoSavannaNFT",
+    discord: "https://discord.gg/RHINOSAVANNANFT",
+    symbol: "rhino_savanna",
+    supply: 1560,
+    preminted: 52,
+    price: "1.9",
+    wlPrice: "2.4",
+    description: `Rhino Savanna is a project with a big heart, aimed at promoting awareness
+    of endangered species and climate change. With over 8933 passionately designed Rhinos,
+    incorporating cultures and countries from all over the world, owning a Rhino symbolizes
+    the mentality of global oneness.
+
+    We're partnered with the Wildlife Conservation Network's Rhino Recovery Fund and
+    International Rhino Foundation. We'll be donating 10% of Mint price for EACH minted
+    NFT to be used towards Rhino protection, health and habitats!`,
   },
 ];
 
@@ -150,14 +67,14 @@ export const minting_collections = [
     supply: 6969,
     preminted: 50,
     price: 0.25,
-    description: `They say you can’t always have angel by your side. Guess what? Well, 
-    now you can! We have created these 6969 truly unique hand drawn angels that consist 
-    of various traits with own special rarity level. There are also some secrets hidden 
-    in the world and NFTs itself. Join Cutiez community to combine them all and solve a 
-    collection mystery which will unlock bigger rewards, special airdrop and much more 
-    to come. CutiezAngels will initiate evolution of Ridralas MetaWorld which is available 
-    for you to observe on the main website of Cutiez Metaverse. We want to use our NFTs in 
-    future merch store and P2E online TD game! This is what Solana NFT world needs... a 
+    description: `They say you can’t always have angel by your side. Guess what? Well,
+    now you can! We have created these 6969 truly unique hand drawn angels that consist
+    of various traits with own special rarity level. There are also some secrets hidden
+    in the world and NFTs itself. Join Cutiez community to combine them all and solve a
+    collection mystery which will unlock bigger rewards, special airdrop and much more
+    to come. CutiezAngels will initiate evolution of Ridralas MetaWorld which is available
+    for you to observe on the main website of Cutiez Metaverse. We want to use our NFTs in
+    future merch store and P2E online TD game! This is what Solana NFT world needs... a
     Cutiez Metaverse. For more information please visit our website and Discord server!`,
   },
 ];
